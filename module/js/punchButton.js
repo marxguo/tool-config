eb6f63a94c0bb06f381df99762d3f9b0203aa62c
marxguo <134024760+marxguo@users.noreply.github.com>
2023-05-24 22:46:32 +0800

    Update punchButton.js

--- module/js/punchButton.js
+++ module/js/punchButton.js
@@ -18,25 +18,25 @@ const paramsArr = body.split('&');
 for (let i = 0; i < paramsArr.length; i++) {
   const [key, value] = paramsArr[i].split('=');
   if (key === 'deviceInfo') {
-    paramsArr[i] = 'deviceInfo={"status":"1","SSID":"EN+","networkType":"wifi","BSSID":"58:b3:8f:72:14:50"}';
+    paramsArr[i] = `deviceInfo=${encodeURIComponent('{"status":"1","SSID":"EN+","networkType":"wifi","BSSID":"58:b3:8f:72:14:50"}')}`;
   }
   if (key === 'mac') {
     paramsArr[i] = 'mac=58:b3:8f:72:14:50';
   }
   if (key === 'sid') {
-    paramsArr[i] = 'sid=EN+';
+    paramsArr[i] = `sid=${encodeURIComponent('EN+')}`;
   }
   if (key === 'positionInfo') {
-    paramsArr[i] = 'positionInfo={"status":"1","err_Info":"success","longitude":113.9332,"speed":-1,"latitude":22.63625,"accuracy":35,"errMsg":"getLocation:ok","address":"广东省深圳市南山区西丽街道松白路1026号深圳市南岗第二工业园"}';
+    paramsArr[i] = `positionInfo=${encodeURIComponent('{"status":"1","err_Info":"success","longitude":113.9332,"speed":-1,"latitude":22.63625,"accuracy":35,"errMsg":"getLocation:ok","address":"广东省深圳市南山区西丽街道松白路1026号深圳市南岗第二工业园"}')}`;
   }
   if (key === 'longitude') {
-    paramsArr[i] = 'longitude=113.9332';
+    paramsArr[i] = `longitude=${encodeURIComponent('113.9332')}`;
   }
   if (key === 'latitude') {
-    paramsArr[i] = 'latitude=22.63625';
+    paramsArr[i] = `latitude=${encodeURIComponent(22.63625)}`;
   }
   if (key === 'position') {
-    paramsArr[i] = 'position=广东省深圳市南山区西丽街道松白路1026号深圳市南岗第二工业园';
+    paramsArr[i] = `position=广东省深圳市南山区西丽街道松白路1026号深圳市南岗第二工业园`;
   }
 }
 const newBody = paramsArr.join('&');
