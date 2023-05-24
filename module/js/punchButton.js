f0b1ef697f73daef9d68914fbd81eaee2c4ce5a1
marxguo <134024760+marxguo@users.noreply.github.com>
2023-05-24 21:21:37 +0800

    Update punchButton.js

--- module/js/punchButton.js
+++ module/js/punchButton.js
@@ -1,5 +1,5 @@
 //http://oa.en-plus.com.cn:8090/api/hrm/kq/attendanceButton/punchButton
-let obj = JSON.parse($request.body);
+let obj = JSON.parse($request.params);
 
 obj.deviceInfo = {"status":"1","SSID":"EN+","networkType":"wifi","BSSID":"58:b3:8f:72:14:50"};
 obj.mac = '58:b3:8f:72:14:50';
@@ -9,4 +9,4 @@ obj.longitude = 113.9332;
 obj.latitude = 22.63625;
 obj.position = '广东省深圳市南山区西丽街道松白路1026号深圳市南岗第二工业园';
   
-$done({body: JSON.stringify(obj)});
+$done({params: JSON.stringify(obj)});
