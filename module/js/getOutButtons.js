9d3144914229adfd48907a04c5fe50b1b0ff575b
marxguo <134024760+marxguo@users.noreply.github.com>
2023-05-24 09:23:52 +0800

    Update getOutButtons.js

--- module/js/getOutButtons.js
+++ module/js/getOutButtons.js
@@ -1,1 +1,13 @@
 // http://oa.en-plus.com.cn:8090/api/hrm/kq/attendanceButton/getOutButtons
+let obj = JSON.parse($response.body);
+
+obj = {
+  "isrange" : "1",
+  "status" : "1",
+  "locationid" : "",
+  "rangename" : "",
+  "rangekey" : "",
+  "outsidesign" : "0"
+};
+  
+$done({body: JSON.stringify(obj)});
