1858d189d31e6825b67689bc1d8ea8a5255aa7c0
marxguo <134024760+marxguo@users.noreply.github.com>
2023-05-24 18:35:06 +0800

    Update getLocationWifiInfo.js

--- module/js/getLocationWifiInfo.js
+++ module/js/getLocationWifiInfo.js
@@ -3,27 +3,26 @@ let obj = JSON.parse($response.body);
 
 obj = {
   "wifiInfo" : {
-    "wifis" : [
-
-    ],
-    "wificheck" : true
-  },
-  "status" : "1",
-  "locationInfo" : {
-    "locationcheckscope" : "300",
-    "locations" : [
-      {
-        "address" : "松白路1026号未来科学城6栋2-3层",
-        "checkscope" : "100",
-        "groupid" : "1",
-        "id" : "1",
-        "longitude" : "113.933159",
-        "locationname" : "深圳驿普乐氏科技有限公司",
-        "latitude" : "22.635843"
-      }
-    ],
-    "locationcheck" : false
-  }
+    "wifiInfo": {
+        "wifis": [],
+        "wificheck": true
+    },
+    "locationInfo": {
+        "locationcheckscope": "300",
+        "locations": [
+            {
+                "checkscope": "100",
+                "locationname": "深圳驿普乐氏科技有限公司",
+                "address": "松白路1026号未来科学城6栋2-3层",
+                "latitude": "22.635843",
+                "groupid": "1",
+                "id": "1",
+                "longitude": "113.933159"
+            }
+        ],
+        "locationcheck": false
+    },
+    "status": "1"
 };
 
 $done({body: JSON.stringify(obj)});
