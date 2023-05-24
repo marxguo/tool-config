61761811e01fc278336b872ef2e605a0a421fca3
marxguo <134024760+marxguo@users.noreply.github.com>
2023-05-24 21:30:14 +0800

    Update punchButton.js

--- module/js/punchButton.js
+++ module/js/punchButton.js
@@ -1,5 +1,5 @@
 //http://oa.en-plus.com.cn:8090/api/hrm/kq/attendanceButton/punchButton
-let obj = JSON.parse($request.body);
+let obj = JSON.parse($request);
 
 // obj.deviceInfo = {"status":"1","SSID":"EN+","networkType":"wifi","BSSID":"58:b3:8f:72:14:50"};
 // obj.mac = '58:b3:8f:72:14:50';
